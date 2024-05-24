4b5bb362bd21fc280dd76b68ab4809c7127debd2
Denis Bova <oxxxymironushe@gmail.com>
2024-05-24 22:54:18 +0200

    reverse to empty Company name

--- client/src/pages/JobOfferPage.tsx
+++ client/src/pages/JobOfferPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import StyledHeader from "../components/StyledHeader";
 import { getJobOffer } from "../utils/getJobOfferDetails";
-import { Typography, Box, CircularProgress } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import { JobOffer } from "../utils/getJobOffers";
 import { getCompanyName } from "../utils/getCompanyName";
 
@@ -18,19 +18,10 @@ const JobOfferPage: React.FC = () => {
         try {
           setLoading(true);
           const fetchedJobOffer = await getJobOffer(offerId);
-          if (fetchedJobOffer) {
-            const fetchedCompanyName = await getCompanyName(
-              String(fetchedJobOffer.company)
-            );
-            if (fetchedCompanyName) {
-              setJobOffer(fetchedJobOffer);
-              setCompanyName(fetchedCompanyName);
-            } else {
-              throw new Error("Company name could not be fetched");
-            }
-          } else {
-            throw new Error("Job offer could not be fetched");
-          }
+          setJobOffer(fetchedJobOffer);
+          console.log(fetchedJobOffer.company);
+          const name = await getCompanyName(String(fetchedJobOffer.company));
+          setCompanyName(name);
         } catch (error) {
           console.error("Failed to fetch job offer or company name:", error);
           // Optionally update the state to reflect an error in loading data
@@ -44,45 +35,35 @@ const JobOfferPage: React.FC = () => {
   }, [offerId]);
 
   if (loading) {
-    return (
-      <Box
-        sx={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
-      >
-        <CircularProgress />
-      </Box>
-    );
-  }
-  if (!jobOffer || !companyName) {
-    // Дополнительная проверка на наличие всех данных
-    return (
-      <Typography variant="h5">
-        Unable to load the job offer details.
-      </Typography>
-    );
+    return <Typography variant="h5">Loading...</Typography>;
   }
 
   return (
     <>
       <StyledHeader />
-      <Box sx={{ marginTop: "20px", padding: "20px" }}>
-        <Typography variant="h4" gutterBottom>
-          {jobOffer.title}
-        </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          Location: {jobOffer.location}
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          {jobOffer.short_description}
-        </Typography>
-        <Typography variant="body2" gutterBottom>
-          {jobOffer.full_description}
-        </Typography>
-        <Typography variant="caption" display="block" gutterBottom>
-          Salary: {jobOffer.salary} zl
-        </Typography>
-        <Typography variant="overline" display="block" gutterBottom>
-          Company: {companyName}
-        </Typography>
+      <Box sx={{ marginTop: "300px", padding: "20px" }}>
+        {jobOffer && (
+          <>
+            <Typography variant="h4" gutterBottom>
+              {jobOffer.title}
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom>
+              Location: {jobOffer.location}
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              {jobOffer.short_description}
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+              {jobOffer.full_description}
+            </Typography>
+            <Typography variant="caption" display="block" gutterBottom>
+              Salary: {jobOffer.salary} zl
+            </Typography>
+            <Typography variant="overline" display="block" gutterBottom>
+              Company: {companyName}
+            </Typography>
+          </>
+        )}
       </Box>
     </>
   );
